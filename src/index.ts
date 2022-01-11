544f7eade69c9b6c5a997525f55a8d56517add90
Gustavo Henrique <gu.bat98@gmail.com>
2022-01-11 01:19:32 -0300

    feat: index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import FakeCommiter from './lib/FakeCommiter'
+import chalk from 'chalk'
+import minimist from 'minimist'
+const argv = minimist(process.argv.slice(2))
+
+const fakeCommiter = new FakeCommiter({
+  u: argv.u,
+  t: argv.t,
+  from: argv.from,
+  to: argv.to,
+  verbose: argv.verbose
+})
+
+fakeCommiter
+  .fake()
+  .then(() => console.log(chalk.green(`Faked!\nNow, run 'git push' to push your faked commits`)))
